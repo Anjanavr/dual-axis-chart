b08c7ff5d7fcc9e8d85bb84a9da79a8f4aa957a9
anjanavr <anjana@qburst.com>
2018-03-08 14:04:14 +0530

    mixed chart is drawn with hard coded data

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import Chart from 'chart.js';
 
-
+declare var Chart: any;
 @Component({
     selector: 'app-chart',
     templateUrl: './chart.component.html',
@@ -9,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 
+    chart: any;
+
     constructor() { }
 
     ngOnInit() {
+        this.drawChart();
+    }
+
+    drawChart () {
+        const canvas = document.getElementById('chart');
+        this.chart = new Chart(canvas, {
+        type: 'bar',
+        data: {
+            labels: ['1', '2', '3', '4', '5'],
+            datasets: [{
+            label: 'A',
+            yAxisID: 'A',
+            data: [100, 96, 84, 76, 69]
+            }, {
+            label: 'B',
+            yAxisID: 'B',
+            data: [2, 6, 4, 5, 0],
+            type: 'line'
+            }, {
+                label: 'C',
+                yAxisID: 'A',
+                data: [45, 96, 74, 85, 20]
+            }]
+        },
+        options: {
+            scales: {
+            yAxes: [{
+                id: 'A',
+                type: 'linear',
+                position: 'left',
+            }, {
+                id: 'B',
+                type: 'linear',
+                position: 'right',
+                ticks: {
+                max: 10,
+                min: 0
+                }
+            }]
+            }
+        }
+        });
     }
 
 }
