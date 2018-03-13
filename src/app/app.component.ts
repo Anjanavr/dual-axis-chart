b803aaa9b5cd5c34bcf70a8d57d892be9b74e6d4
anjanavr <anjana@qburst.com>
2018-03-13 18:11:51 +0530

    modified code according to the code review comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,80 +1,28 @@
 import { Component, ViewChild, AfterViewInit } from '@angular/core';
 
-import { AppConstants } from './shared/constants';
-import { FileService } from './shared/util';
-
 import { ChartComponent } from './chart/chart.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    providers: [
-        AppConstants,
-        FileService
-    ]
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit {
     title = 'app';
 
-    @ViewChild('fileImportInput')
-
-    fileImportInput: any;
     @ViewChild(ChartComponent) chartComponent: ChartComponent;
     csvRecords = [];
     chartConfig: any;
 
     constructor(
-        private _fileUtil: FileService
     ) { }
 
     ngAfterViewInit() {
     }
 
-    // METHOD CALLED WHEN CSV FILE IS IMPORTED
-    fileChangeListener($event): void {
-
-        const text = [];
-        const files = $event.srcElement.files;
-
-        if (AppConstants.validateHeaderAndRecordLengthFlag) {
-            if (!this._fileUtil.isCSVFile(files[0])) {
-                alert('Please import valid .csv file.');
-                this.fileReset();
-            }
-        }
-
-        const input = $event.target;
-        const reader = new FileReader();
-        reader.readAsText(input.files[0]);
-
-        reader.onload = (data) => {
-            const csvData = reader.result;
-            const csvRecordsArray = csvData.split(/\r\n|\n/);
-
-            let headerLength = -1;
-            if (AppConstants.isHeaderPresentFlag) {
-                const headersRow = this._fileUtil.getHeaderArray(csvRecordsArray, AppConstants.tokenDelimeter);
-                headerLength = headersRow.length;
-            }
-            this.csvRecords = this._fileUtil.getDataRecordsArrayFromCSVFile(csvRecordsArray,
-                headerLength, AppConstants.validateHeaderAndRecordLengthFlag, AppConstants.tokenDelimeter);
-            if (this.csvRecords == null) {
-                // If control reached here it means csv file contains error, reset file.
-                this.fileReset();
-            } else {
-                this.createChart();
-            }
-        };
-
-        reader.onerror = function () {
-            alert('Unable to read ' + input.files[0]);
-        };
-    }
-
-    fileReset() {
-        this.fileImportInput.nativeElement.value = '';
-        this.csvRecords = [];
+    uploadCompleted(data) {
+        this.csvRecords = data;
+        this.createChart();
     }
 
     createChart() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule, NgModel } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ChartComponent } from './chart/chart.component';
+import { CsvUploadComponent } from './csv-upload/csv-upload.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        ChartComponent
+        ChartComponent,
+        CsvUploadComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        FormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
